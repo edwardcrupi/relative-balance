60e73b54f74fec008f174c683059b5a59829aaea
Edward Crupi <e.crupi@student.unimelb.edu.au>
2019-11-14 02:13:29 +1100

    Update test to actually test properly

--- RelativeBalanceTest.java
+++ RelativeBalanceTest.java
@@ -1,11 +1,13 @@
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.io.IOException;
+import java.text.ParseException;
 public class RelativeBalanceTest {
-  public static void main(String[] args) throws IOException {
-    String result =  RelativeBalance.readTransactions(new Date("20/10/2018 12:00:00"), new Date("20/10/2018 17:00:00"), "ACC334455", "data/test.csv", new SimpleDateFormat("dd/MM/yyyy HH:mm:ss"));
-    assert result.contains("-$25.0")
-    && result.contains("1");
+  public static void main(String[] args) throws IOException, ParseException {
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    String result =  RelativeBalance.readTransactions(formatter.parse("20/10/2018 12:00:00"), formatter.parse("20/10/2018 17:00:00"), "ACC334455", "./data/test.csv", formatter);
+    assert(result.contains("-$25.0")
+    && result.contains("1"));
     System.out.println("Test passed");
     return;
   }
